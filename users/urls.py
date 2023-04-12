ac54c7f3e09ab9bc1cd88baa7ca562066447558d
Shivansh <shuklashivansh26@gmail.com>
2023-04-12 07:00:49 +0530

    Map notif views to urls

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import AuthenticateView, PhoneView, UserDetailView, AllUsersView
+from .views import AuthenticateView, PhoneView, UserDetailView, AllUsersView, AddSubscription, RemoveSubscription
 
 urlpatterns = [
     path('auth', AuthenticateView.as_view(), name='authenticate'),
     path('phone', PhoneView.as_view(), name='phone'),
-    path('all', AllUsersView.as_view(), name='all_userse'),
+    path('all', AllUsersView.as_view(), name='all_users'),
+    path('subscribe', AddSubscription.as_view(), name='subscribe_notifs'),
+    path('unsubscribe', RemoveSubscription.as_view(), name='unsubscribe_notifs'),
     path('', UserDetailView.as_view(), name='user_detail'),
 ]
